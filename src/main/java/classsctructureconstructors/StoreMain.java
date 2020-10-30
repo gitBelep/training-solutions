21302fe268c45382496a369c158ad1765e1bf1ac
gitBelep <ddawid3@freemail.hu>
2020-10-30 18:57:48 +0100

    Raktár-takarékosabban

--- src/main/java/classsctructureconstructors/StoreMain.java
+++ src/main/java/classsctructureconstructors/StoreMain.java
@@ -10,28 +10,28 @@ public class StoreMain {
         Store store1 = new Store(name1);
 
         System.out.println("Második Terméked:");
-        Scanner sc2 = new Scanner(System.in);
-        String name2 = sc2.nextLine();
+     //   Scanner sc2 = new Scanner(System.in);
+        String name2 = sc1.nextLine();
         Store store2 = new Store(name2);
 
         System.out.println("Készletnövelés " + store1.getProduct() );
-        Scanner sc3 = new Scanner(System.in);
-        int sc13 = sc3.nextInt();
+     //   Scanner sc3 = new Scanner(System.in);
+        int sc13 = sc1.nextInt();
         store1.store(sc13);
 
         System.out.println("Készletnövelés " + store2.getProduct() );
-        Scanner sc4 = new Scanner(System.in);
-        int sc14 = sc4.nextInt();
+    //    Scanner sc4 = new Scanner(System.in);
+        int sc14 = sc1.nextInt();
         store2.store(sc14);
 
         System.out.println("Kiszállítás " + store1.getProduct() );
-        Scanner sc5 = new Scanner(System.in);
-        int sc15 = sc5.nextInt();
+     //   Scanner sc5 = new Scanner(System.in);
+        int sc15 = sc1.nextInt();
         store1.dispatch(sc15);
 
         System.out.println("Kiszállítás " + store2.getProduct() );
-        Scanner sc6 = new Scanner(System.in);
-        int sc16 = sc6.nextInt();
+     //   Scanner sc6 = new Scanner(System.in);
+        int sc16 = sc1.nextInt();
         store2.dispatch(sc16);
 
         System.out.println(store1.getProduct() +": " + store1.getStock());

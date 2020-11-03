3c88b4b949b81c29b1e2e6f7c155fdfa1e07809a
gitBelep <ddawid3@freemail.hu>
2020-11-03 12:25:26 +0100

    different packages

---
+++ src/main/java/packages/main/MainProgramm.java
@@ -0,0 +1,11 @@
+package packages.main;
+
+import java.util.Scanner;
+
+public class MainProgramm {
+    public static void main(String[] args) {
+        packages.greetings.Greeter g = new packages.greetings.Greeter();
+        g.sayHello();
+        Scanner scanner;
+    }
+}

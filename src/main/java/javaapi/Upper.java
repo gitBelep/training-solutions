e2163ab0e8e159ca39e533d4a1cf41cf4633852c
gitBelep <ddawid3@freemail.hu>
2020-11-03 14:39:13 +0100

    NAGYBÖTŰS

---
+++ src/main/java/javaapi/Upper.java
@@ -0,0 +1,12 @@
+package javaapi;
+
+public class Upper {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        System.out.println(str.toUpperCase());
+        System.out.println("String\tnextLine()\t\n" +
+                "Advances this scanner past the current line and returns the input that was skipped.");
+        System.out.println("int\tnextInt()\t\n" +
+                "Scans the next token of the input as an int.");
+    }
+}

3c839f5993ee120de14042e71668f3df1b543689
gitBelep <ddawid3@freemail.hu>
2020-11-04 15:02:56 +0100

    product

---
+++ src/main/java/week02/Product.java
@@ -0,0 +1,34 @@
+package week02;
+class Product {
+    private String name;
+    private String code;
+
+    public Product(String name, String code){
+        this.name = name;
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String getCode() {
+        return code;
+    }
+    public boolean areTheyEqual(Product p){
+        boolean first = name.equals(p.name);
+        boolean second = code.length() - p.code.length()<2 && code.length() - p.code.length() >-2;
+        return first && second;
+    }
+
+    public static void main(String[] args) {
+        Product p1 = new Product("vaj", "2222");
+        Product p2 = new Product("vaj","333322");
+        Product p3 = new Product("gél","2244");
+        Product p4 = new Product("gél","gél*");
+
+        System.out.println(p1.areTheyEqual(p2));
+        System.out.println(p1.areTheyEqual(p3));
+        System.out.println(p2.areTheyEqual(p3));
+        System.out.println(p3.areTheyEqual(p4));
+    }
+}

68eade68e1555f9a5848c87a2c15a326ce59f448
gitBelep <ddawid3@freemail.hu>
2020-11-09 23:02:34 +0100

    opcionális nem ment

---
+++ src/main/java/primitivetypes/PrimitiveTypes.java
@@ -0,0 +1,17 @@
+package primitivetypes;
+
+public class PrimitiveTypes {
+
+    public static void main(String[] args) {
+        int a = 5;
+        Integer b = 3;
+        System.out.println(Integer.toBinaryString(a));
+        System.out.println(Integer.toBinaryString(b));
+        System.out.println(new Integer(1) + new Integer(2));
+        Integer i =7;
+        Integer k= 5;
+        System.out.println(i+k);
+
+    }
+
+}

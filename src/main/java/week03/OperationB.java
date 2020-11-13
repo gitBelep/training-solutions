969a9b781ca0aa47f845f44b0f978b30a65267f9
gitBelep <ddawid3@freemail.hu>
2020-11-13 19:43:13 +0100

    több kivétek kezelése

---
+++ src/main/java/week03/OperationB.java
@@ -0,0 +1,32 @@
+package week03;
+import java.util.Scanner;
+
+public class OperationB {
+        private int leftValue;
+        private int rightValue;
+
+        public OperationB(String str) {
+            this.leftValue = Integer.parseInt(str.substring(0, str.indexOf("+")).trim());
+            this.rightValue = Integer.parseInt(str.substring(str.indexOf("+")+1).trim());
+        }
+
+        public int getResult(){
+            return leftValue+rightValue;
+        }
+
+        public static void main(String[] args) {
+            OperationB opp = null;
+            Scanner sc = new Scanner(System.in);
+            while (opp == null) {
+                System.out.println("Összeadás ~  int + int formátumban");
+                String muvelet = sc.nextLine();
+                try {
+                    opp = new OperationB(muvelet);
+                } catch (IllegalArgumentException | StringIndexOutOfBoundsException ie) {
+//  throw new IllegalArgumentException("Helytelen formátum! Újra: ");  -- erre kilépne
+                    System.out.println(ie);
+                }
+            }
+            System.out.println("Eredmény: "+ opp.getResult());
+        }
+}

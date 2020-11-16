bf289ae08df1928cd2115a1b319977f058b9569c
gitBelep <ddawid3@freemail.hu>
2020-11-16 21:14:05 +0100

    Week04

---
+++ src/main/java/week04/NameChanger.java
@@ -0,0 +1,20 @@
+package week04;
+
+public class NameChanger {
+    private String fullName;
+
+    public NameChanger(String fullName) {
+        if (fullName == null || "".equals(fullName.trim())){
+            throw new IllegalArgumentException("Invalid Name:" + fullName);
+        }
+        this.fullName = fullName;
+    }
+    public void changeFirstName(String firstName){
+        String lastName = fullName.split(" ")[1];
+        this.fullName = firstName +" " +lastName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+}
---
+++ src/test/java/week04/NameChangerTest.java
@@ -0,0 +1,21 @@
+package week04;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class NameChangerTest {
+
+    @Test
+    public void makeNcTest() {
+        NameChanger nc = new NameChanger("");
+        assertThrows(IllegalArgumentException.class, () -> new NameChanger(""));
+  }
+
+    @Test
+    public  void changeFirstNameTest(){
+        NameChanger nc = new NameChanger("Ede Kovi");
+        nc.changeFirstName("Anna");
+        assertEquals("Anna Kovi", nc.getFullName());
+    }
+}

5371034fe2eebc3bd6f1cacb8b54299c7a1d55e5
gitBelep <ddawid3@freemail.hu>
2020-11-18 16:15:50 +0100

    guess

--- src/main/java/week04/NumberGuesser.java
+++ src/main/java/week04/NumberGuesser.java
@@ -5,21 +5,44 @@ public class NumberGuesser {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int rnd = (int) (Math.random()*100+1);
-        System.out.println("csalás: "+ rnd);
-        do{
-            System.out.println("Tippelj egy számot 1 és 100 közt");
-            int gu = Integer.parseInt(sc.nextLine());
+        int kor =2;
+        String out = null;
+        System.out.println("JÁTÉK    by dd\n   >>Találd ki a számot<<\nkilépés \"q\"-val.");
+        System.out.println("\nTippelj egy számot 1 és 100 közt!\n7 lehetőséged van");
 
-            if (gu == rnd){
-                System.out.println("Találat!");
-                rnd = (int) (Math.random()*100+1);
-            }else{
-                if (gu > rnd){
-                    System.out.println("Kisebb számra gondoltam");
-                }else{
-                    System.out.println("Nagyobb számra gondoltam");
+        do {
+            int gu =-1001;
+            while (gu == -1001) {
+                try {
+                    out = sc.nextLine();              //Beolvasás
+                    if ("q".equals(out)){break;}      //belsőből kiugrik
+                    gu = Integer.parseInt(out);
+                } catch (NumberFormatException nfe) {
+                    System.out.println("Számra gondoltam!!  Nos?");
                 }
             }
-        }while (!sc.equals("q"));
+            if ("q".equals(out)){break;}
+
+            if (gu == rnd){                            //Találat?
+                System.out.println(" *  G r a t u l á l o k !  K i t a l á l t a d !  *\n");
+                System.out.println("Tippelj egy számot 1 és 100 közt!\n7 lehetőséged van");
+                rnd = (int) (Math.random()*100+1);
+                kor = 2;
+                continue;
+            }else{
+                if (gu > rnd){
+                    System.out.println("Kisebb számra gondoltam..");
+                }else{
+                    System.out.println("Nagyobb számra gondoltam..");
+                }
+            }
+
+            if (kor == 8){
+                System.out.println("Ez most nem sikerült  :-( \n   * * Vége * *");
+                break;
+            }
+            System.out.println(kor +(". kör.  Új tipped?"));
+            kor++;
+        }while (true);
     }
 }

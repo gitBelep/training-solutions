1b62f1c3784ee57e4dfca99dea5eb70d6cdb7a84
gitBelep <ddawid3@freemail.hu>
2020-11-27 13:25:00 +0100

    user

---
+++ src/main/java/week05d05/User.java
@@ -0,0 +1,25 @@
+package week05d05;
+
+public class User {
+    private String firstName;
+    private String lastName;
+    private String eMail;
+
+    public User(String firstName, String lastName, String eMail){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        if (eMail.contains(".") && eMail.contains("@")) {
+            this.eMail = eMail;
+        } else {
+            throw new IllegalArgumentException("e-mail should contain \".\" and \"@\" !");
+        }
+    }
+
+    public String geteMail() {
+        return eMail;
+    }
+
+    public String getFullName(){
+        return firstName +" "+ lastName;
+    }
+}
---
+++ src/test/java/week05d05/UserTest.java
@@ -0,0 +1,35 @@
+package week05d05;
+
+import org.junit.jupiter.api.Test;
+import stringmethods.filename.FileNameManipulator;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class UserTest {
+    @Test
+    public void getUserFullname2() {
+        User u = new User("Aba", "Béla", "er@freemail.ru");
+
+        assertEquals("Aba Béla",u.getFullName());
+    }
+    @Test
+    public void emailOk2() {
+        User u = new User("Aba", "Béla", "er@freemail.ru");
+        assertEquals("er@freemail.ru", u.geteMail());
+    }
+    @Test
+    public void emailDoesntContainsAt() {
+        Exception ex = assertThrows(IllegalArgumentException.class, () -> {
+            new User("Aba", "Béla", "er.freemail.ru");
+        });
+        assertEquals("e-mail should contain \".\" and \"@\" !", ex.getMessage());
+    }
+    @Test
+    public void emailDoesntContainsDot() {
+        Exception ex = assertThrows(IllegalArgumentException.class, () -> {
+            new User("Aba", "Béla", "er@freemail@ru");
+        });
+        assertEquals("e-mail should contain \".\" and \"@\" !", ex.getMessage());
+    }
+}

2e8e3fe5d3cdfe9b2fbbc621d6e6f1dea1b5b90d
gitBelep <ddawid3@freemail.hu>
2020-11-29 12:06:53 +0100

    .

--- src/main/java/algorithmsdecision/bankaccounts/TransactionDecisionMaker.java
+++ src/main/java/algorithmsdecision/bankaccounts/TransactionDecisionMaker.java
@@ -3,7 +3,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public class TransactionDecisionMaker {
-    
+
     public boolean containsCreditEntryGreaterThan(List<Transaction> transactions, LocalDateTime startTime, LocalDateTime endTime, int minAmount){
         for (Transaction t : transactions){
             if (t.getAmount() > minAmount && t.isDebit()){

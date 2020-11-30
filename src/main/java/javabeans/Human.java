706c56e97960e99cbe84184a2f3d833309ccf251
gitBelep <ddawid3@freemail.hu>
2020-11-30 15:53:37 +0100

    beans

---
+++ src/main/java/javabeans/Human.java
@@ -0,0 +1,35 @@
+package javabeans;
+
+public class Human {
+
+    private String name;
+    private int iq;
+    private double weight;
+
+    public Human() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getIq() {
+        return iq;
+    }
+
+    public void setIq(int iq) {
+        this.iq = iq;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+}

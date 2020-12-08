60e1d8ab5de4671790dde4bab572152f2eb34fad
gitBelep <ddawid3@freemail.hu>
2020-12-08 18:23:53 +0100

    sum digits -2 ways

--- src/main/java/week07d02/DigitSum.java
+++ src/main/java/week07d02/DigitSum.java
@@ -1,17 +1,25 @@
 package week07d02;
-
 import static java.lang.Integer.parseInt;
 
 public class DigitSum {
-    public int sumOfDigits(int x){
+    public int sumOfDigits(int x) {
         String strNumber = Integer.toString(x);
         int sum = 0;
         String ss = "";
         Character c = null;
-        for (int i = 0; i < strNumber.length();i++){
-            c =(Character) strNumber.charAt(i);
+        for (int i = 0; i < strNumber.length(); i++) {
+            c = (Character) strNumber.charAt(i);
             ss = Character.toString(c);
             sum += parseInt(ss);
+        }
+        return sum;
+    }
+
+    public static int sumOfDigitsMuchBetterIdea(int x) {
+        int sum = 0;
+        while (x != 0) {
+            sum = sum + x % 10;
+            x = x / 10;
         }
         return sum;
     }
--- src/test/java/week07d02/DigitSumTest.java
+++ src/test/java/week07d02/DigitSumTest.java
@@ -1,16 +1,21 @@
 package week07d02;
-
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DigitSumTest {
-@Test
+    @Test
     public void sumTest(){
-    DigitSum d = new DigitSum();
-    assertEquals(9, d.sumOfDigits(234));
-    assertEquals(16, d.sumOfDigits(934));
-    assertEquals(2, d.sumOfDigits(200000));
-}
+        DigitSum d = new DigitSum();
+        assertEquals(9, d.sumOfDigits(234));
+        assertEquals(16, d.sumOfDigits(934));
+        assertEquals(2, d.sumOfDigits(200000));
+    }
+
+    @Test
+    public void sumTestMuchBetterIdea(){
+        assertEquals(9, DigitSum.sumOfDigitsMuchBetterIdea(234));
+        assertEquals(16, DigitSum.sumOfDigitsMuchBetterIdea(934));
+        assertEquals(2, DigitSum.sumOfDigitsMuchBetterIdea(200000));
+    }
 }
 

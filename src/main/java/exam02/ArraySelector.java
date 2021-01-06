e40753c144bd7554c43871670b5dd99b869797df
gitBelep <ddawid3@freemail.hu>
2021-01-06 18:34:29 +0100

    exam2 - ArraySelector - kűködik

---
+++ src/main/java/exam02/ArraySelector.java
@@ -0,0 +1,18 @@
+package exam02;
+
+public class ArraySelector {
+
+    public String selectEvens(int[] incomingList) {
+        String result = "[";
+        if (incomingList.length == 0){
+            return new String("");
+        }
+        
+        for (int i = 0; i < incomingList.length; i=i+2){
+            result = result + incomingList[i] +", ";
+        }
+        result = result.substring(0, result.length()-2) + "]";
+        return result;
+    }
+
+}

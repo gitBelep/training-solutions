d83794de6ae8ad27d8371b57f1532a339c4a8bca
gitBelep <ddawid3@freemail.hu>
2021-01-11 14:46:16 +0100

    számjegyei osztják-e a számot

--- src/main/java/week11d01/DivisorFinder.java
+++ src/main/java/week11d01/DivisorFinder.java
@@ -2,23 +2,46 @@ package week11d01;
 
 public class DivisorFinder {
 
-    public int findDivisors(int n){
+//saját:
+    public int findDivisors(int n){  //negatívnál 0 fut rajta végig
         int number = n;
         String strNr =""+n;
-        int szamJegy = 0;
+        int digit;
         int counter =0;
 
         for (int i = strNr.length(); i > 0; i--){
-            szamJegy = number - (number / 10) * 10;
-            if (szamJegy == 0){
+            digit = number - (number / 10) * 10;
+            if (digit == 0){             //0-val osztásként étrékeli az "int%0"-t.
                 number = number / 10;
                 continue;
             }
-            if (n % szamJegy == 0){
+            if (n % digit == 0){
                 counter++;
             }
             number = number / 10;
         }
         return counter;
     }
+
+// órai
+    public int findDivisors2(int n) {
+        int number;
+        number = n < 0 ? -1 * n : n;  //negatívot pozitívvá kell tenni, vagy feltétel kell..
+        int counter = 0;
+        String strNr = Integer.toString(number);
+        char[] charNr = strNr.toCharArray(); //vagy Integer.parseInt(substring(i,i+1))
+
+        for (char c : charNr) {
+            if (Integer.parseInt((String.valueOf(c))) == 0){
+                continue;
+            }
+            if (number % Integer.parseInt((String.valueOf(c))) == 0) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+//Megnézzük a szám minden egyes számjegyére, hogy osztója-e a számnak, majd
+// számoljuk össze őket. Példa: a 425-ben az 5 osztója a számnak, ezért
+// a visszatérési érték 0+0+1 = 1.
 }
--- src/test/java/week11d01/DivisorFinderTest.java
+++ src/test/java/week11d01/DivisorFinderTest.java
@@ -1,5 +1,4 @@
 package week11d01;
-
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,9 +7,23 @@ class DivisorFinderTest {
     @Test
     public void divisorFinder() {
         DivisorFinder d = new DivisorFinder();
-        assertEquals(3, d.findDivisors(12375));
-        assertEquals(2, d.findDivisors(123));
-        assertEquals(2, d.findDivisors(1001));
 
+        assertEquals(3, d.findDivisors(12375));  //1,3,5
+        assertEquals(2, d.findDivisors(-123));   //1,3
+        assertEquals(2, d.findDivisors(-1001));  //1,1
+        assertEquals(1, d.findDivisors(7));
+        assertEquals(1, d.findDivisors(-7));
     }
+
+    @Test
+    public void divisorFinder2() {
+        DivisorFinder d = new DivisorFinder();
+
+        assertEquals(3, d.findDivisors2(12375));
+        assertEquals(2, d.findDivisors2(-123));
+        assertEquals(2, d.findDivisors2(-1001));
+        assertEquals(1, d.findDivisors2(7));
+        assertEquals(1, d.findDivisors2(-7));
+    }
+
 }

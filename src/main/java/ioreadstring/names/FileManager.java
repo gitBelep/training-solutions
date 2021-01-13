157058479588dae347356dbc349ae243075a1962
gitBelep <ddawid3@freemail.hu>
2021-01-13 15:57:04 +0100

    readAllLines()

--- src/main/java/ioreadstring/names/FileManager.java
+++ src/main/java/ioreadstring/names/FileManager.java
@@ -1,8 +1,41 @@
 package ioreadstring.names;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+//vigyázat: file nem a normál resources könyvtárban van!!
+
 public class FileManager {
+    private Path myFile;
+    private List<Human> humans = new ArrayList<>();
+
+    public FileManager(String myFile) {
+        this.myFile = Path.of(myFile);    //miért nem new Path() ??
+    }
 
     public void readFromFile(){
+        try{
+            List<String> humanList = Files.readAllLines(myFile);
+            makeHumans(humanList);
+        } catch (IOException | NullPointerException e) {
+            e.printStackTrace();
+        }
+    }
 
+    private void makeHumans(List<String> strHumans){
+        for (String s : strHumans){
+            String[] names = s.split(" ");
+            humans.add(new Human(names[0], names[1]));
+        }
+    }
+
+    public Path getMyFile() {
+        return myFile;
+    }
+
+    public List<Human> getHumans() {
+        return new ArrayList<>(humans);
     }
 }
--- src/test/java/ioreadstring/names/HumanTest.java
+++ src/test/java/ioreadstring/names/HumanTest.java
@@ -3,16 +3,16 @@ package ioreadstring.names;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-//public class HumanTest {
-//
-//    private Human h = new Human("John", "Doe");
-//
-//
-//    @Test
-//    public void createHumanTest() {
-//        assertEquals("John", h.getFirstName());
-//        assertEquals("Doe", h.getLastName());
-//    }
-//
-//}
+public class HumanTest {
+
+    private Human h = new Human("John", "Doe");
+
+
+    @Test
+    public void createHumanTest() {
+        assertEquals("John", h.getFirstName());
+        assertEquals("Doe", h.getLastName());
+    }
+
+}
 

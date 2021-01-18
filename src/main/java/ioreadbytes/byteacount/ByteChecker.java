1656574d86859405876027b329eecd76b7506121
gitBelep <ddawid3@freemail.hu>
2021-01-18 15:49:41 +0100

    inputStream -félreértésJavítva

--- src/main/java/ioreadbytes/byteacount/ByteChecker.java
+++ src/main/java/ioreadbytes/byteacount/ByteChecker.java
@@ -7,23 +7,52 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class ByteChecker {
+    private static final char CHAR = 'a';
 
     public int readBytesAndFindAs(String file) {
         Path path = Path.of(file);
         try (InputStream ins = new BufferedInputStream(Files.newInputStream(path)) ) {
-            byte[] buffer = new byte[100];
+            byte[] buffer = new byte[1000];
             int size = 0;
-            int turn = 0;
-            int rest = 0;
+            int sum = 0;
             while ( (size = ins.read(buffer)) > 0 ){
-                turn++;
-                rest = size;
+                sum += lookForChar(buffer, CHAR);
             }
-
-            return (turn - 1) * 100 + rest;
+            return sum;
         } catch(IOException ioe) {
             throw new IllegalArgumentException("Cannot read file", ioe);
         }
     }
 
+    private int lookForChar(byte[] buffer, char c){
+        int result = 0;
+        byte searched = (byte)c;
+        for (byte actual : buffer){
+            if (actual == searched){
+                result++;
+            }
+        }
+        return result;
+    }
+
+//A "hivatalos" megoldással is 5063 az eredmény
+//    public int readBytesAndFindAs(String fileName){
+//        Path path  = Path.of(fileName);
+//        int sumOfA =0;
+//        try(InputStream inputStream = Files.newInputStream(path)){
+//            byte[] buffer = new byte[1000];
+//            int size = 0;
+//            while((size=inputStream.read(buffer))>0){
+//
+//                for(byte b : buffer){
+//                    if(b==97){
+//                        sumOfA++;
+//                    }
+//                }
+//            }
+//        } catch (IOException e) {
+//            throw new IllegalStateException("Can't find file!",e);
+//        }
+//        return sumOfA;
+//    }
 }

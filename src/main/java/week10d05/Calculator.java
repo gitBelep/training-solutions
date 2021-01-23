81d7df0450eb402a797a7b949a8b6f45f6824b68
gitBelep <ddawid3@freemail.hu>
2021-01-23 14:45:08 +0100

    Arrays.sort(eztAzArrayt) - senior feladat

--- src/main/java/week10d05/Calculator.java
+++ src/main/java/week10d05/Calculator.java
@@ -1,5 +1,3 @@
-//Lehet Scannerből rögtön Tömbbe pakolni?
-//List -> Array a feladat előírása szerint -> List
 //ez kivételesen a napi feladat Senior változata
 package week10d05;
 
@@ -11,33 +9,16 @@ import java.util.Scanner;
 public class Calculator {
 
     public int[] findMinMaxSum(int[] arr) {
-        List<Integer> numbers = setupList(arr);
-        if (numbers.size() <= 4) {
-            int result = calcValue(numbers);
-            return new int[] {result, result};
-        }
 //itt az ötlet! A max és min kikeresését és kitörlését kb. 40 sor volt leprogramozni!
-        numbers.sort(null);
-        int resultMin = calcValue(numbers.subList(0,4));
-        int s = numbers.size();
-        int resultMax = calcValue(numbers.subList( s-4, s));
-        return new int[] {resultMin, resultMax};
-    }
-
-    private int calcValue(List<Integer> values) {
-        int result = 0;
-        for (int i : values) {
-            result += i;
+        Arrays.sort(arr);
+        int resultMin = 0;
+        int resultMax = 0;
+        int s = arr.length;
+        for (int i = 0; i < s && i < 4; i++) {
+            resultMax += arr[s - 1 - i];
+            resultMin += arr[i];
         }
-        return result;
-    }
-
-    private List<Integer> setupList(int[] arr) {
-        List<Integer> numbers = new ArrayList<>();
-        for (int i : arr) {
-            numbers.add(i);
-        }
-        return numbers;
+        return new int[]{resultMin, resultMax};
     }
 
     public static void main(String[] args) {
@@ -46,18 +27,29 @@ public class Calculator {
         String allTheNumbers = sc1.nextLine();
         Scanner scc = new Scanner(allTheNumbers.replaceAll(" ", "")).useDelimiter(",");
         List<Integer> numbers = new ArrayList<>();
+
+// by Arnold Ádám
+//        String s = scanner.nextLine();
+//        while (!s.isBlank()) {
+//            try {
+//                numbers.add(Integer.parseInt(s));
+//            } catch (NumberFormatException e) {
+//                System.out.println("Számot írj!");
+//            }
+//            s = scanner.nextLine();
+//        }
+
         while (scc.hasNext()) {
             int num = Integer.parseInt(scc.next());
             numbers.add(num);
         }
-
         int[] incomingArray = new int[numbers.size()];
         for (int i = 0; i < numbers.size(); i++) {
             incomingArray[i] = numbers.get(i);
         }
 
         Calculator c = new Calculator();
+        System.out.print("A négy legkisebb, és a négy legnagyobb összege: ");
         System.out.println(Arrays.toString(c.findMinMaxSum(incomingArray)));
     }
 }
-

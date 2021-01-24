0bd295d764a86941cb4d4b83d0ecad90cc2c206f
gitBelep <ddawid3@freemail.hu>
2021-01-24 12:26:56 +0100

    csomagévjavításmegtörténésénekokán

---
+++ src/main/java/initalizer/CreditCard.java
@@ -0,0 +1,48 @@
+package initalizer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CreditCard {
+    public static List<Rate> ACTUAL_RATES = new ArrayList<>();
+    private long balance;
+
+    public CreditCard(long balance, Currency currency, List<Rate> r) {
+        ACTUAL_RATES.addAll(r);
+        this.balance = Math.round(balance * getActualRate(currency));
+    }
+
+    public CreditCard(long balance, Currency currency) {
+        this.balance = Math.round(balance * getActualRate(currency));
+    }
+
+    public CreditCard(long balance) {
+        this.balance = balance;
+    }
+
+    private double getActualRate(Currency currency) {
+        for (Rate r : ACTUAL_RATES) {
+            if (r.getCurrency() == currency) {
+                return r.getConversionFactor();
+            }
+        }
+        return 1;  //HUF..
+    }
+
+    public long getBalance() {
+        return balance;
+    }
+
+    public boolean payment(long amount, Currency currency){
+        if ( balance >= amount * getActualRate(currency) ) {
+            balance -= amount * getActualRate(currency);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean payment(long amount){
+        return payment(amount, Currency.HUF);     //áthív
+    }
+
+}

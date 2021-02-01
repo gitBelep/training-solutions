388f8de68060ca1526616923f51852e6103bfb11
gitBelep <ddawid3@freemail.hu>
2021-02-01 14:38:26 +0100

    Map -benne is REFERENCIA TÍPUSÚ VALUEK VANNAK! Ha megváltoztatom, kész. Visszapakolás nélkül is.

--- src/main/java/week14d01/Indexer.java
+++ src/main/java/week14d01/Indexer.java
@@ -1,21 +1,32 @@
 package week14d01;
-
 import java.util.*;
 
 public class Indexer {
 
-    public Map<Character, List<String>> index(List<String> names){
+    public Map<Character, List<String>> index(List<String> names) {
         Map<Character, List<String>> nameMap = new TreeMap<>();
-        for(String name : names){
+        for (String name : names) {
             Character startLetter = name.charAt(0);
-            if (nameMap.containsKey(startLetter)) {
+            if (!nameMap.containsKey(startLetter)) {
+                nameMap.put(startLetter, new ArrayList<>(Arrays.asList(name)));
+            } else {
                 List<String> existing = nameMap.get(startLetter);
                 existing.add(name);
                 nameMap.put(startLetter, existing);
-            } else {
-                nameMap.put(startLetter, new ArrayList<>(Arrays.asList(name)));
             }
         }
+        return nameMap;
+    }
+
+    public Map<Character, List<String>> indexNincsTulaggodva(List<String> names) {
+        Map<Character, List<String>> nameMap = new TreeMap<>();
+        for (String name : names) {
+            Character startLetter = name.charAt(0);
+            if (!nameMap.containsKey(startLetter)) {
+                nameMap.put(startLetter, new ArrayList<>());  //üresen
+            }
+            nameMap.get(startLetter).add(name);  //cska hozzáadok a List-hez.
+        }                     //nem kell put-tal visszatenni, megváltoztattam
         return nameMap;
     }
 
--- src/test/java/week14d01/IndexerTest.java
+++ src/test/java/week14d01/IndexerTest.java
@@ -1,9 +1,7 @@
 package week14d01;
 
 import org.junit.jupiter.api.Test;
-
 import java.util.*;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class IndexerTest {
@@ -21,4 +19,19 @@ class IndexerTest {
         assertEquals(7, result.size());
         assertEquals("[Gennagyíj, Gazsi]", result.get('G').toString());
     }
+
+    @Test
+    void indexNincsTulaggodva() {
+        Indexer indexer = new Indexer();
+
+        List<String> names = new ArrayList<>(Arrays.asList(
+                "Zénó", "Zoárd", "Álmos", "8", "-", "-8", "Gennagyíj", "Ada", "Ábrahám", "Gazsi"));
+
+        Map<Character, List<String>> result = indexer.index(names);
+        System.out.println(result);
+
+        assertEquals(6, result.size());
+        assertEquals("[Gennagyíj, Gazsi]", result.get('G').toString());
+    }
+
 }

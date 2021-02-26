b9ec096cdf18ae2901c1f3f3964d338645637b2b
gitBelep <ddawid3@freemail.hu>
2021-02-26 23:07:33 +0100

    exam3-2

--- src/main/java/exam03retake02/TodoList.java
+++ src/main/java/exam03retake02/TodoList.java
@@ -8,11 +8,27 @@ public class TodoList {
     private List<Todo> todos = new ArrayList<>();
 
     public int getNumberOfItemsLeft(){
-
+        int counter = 0;
+        for (Todo t : todos){
+            if (t.getState() == State.NON_COMPLETED){
+                counter++;
+            }
+        }
+        return counter;
     }
 
-    public List<Todo> getMostImportantTodosText(){
-
+    public List<String> getMostImportantTodosText(){
+        todos.sort((t1, t2) -> t1.getPriority() - t2.getPriority());
+        List<String> result = new ArrayList<>();
+        result.add(todos.get(0).getText());
+        for (int i = 1; i < todos.size(); i++){
+            if(todos.get(i).getPriority() == todos.get(0).getPriority()){
+                result.add(todos.get(i).getText());
+            } else {
+                break;
+            }
+        }
+        return result;
     }
 
     public void deleteCompleted(){
@@ -23,6 +39,14 @@ public class TodoList {
                 t.remove();
             }
         }
+    }
+
+    public void addTodo(Todo todo){
+        todos.add(todo);
+    }
+
+    public List<Todo> getTodos(){
+        return new ArrayList<>(todos);
     }
 
 }
